fbc63f82a09a25806311944fd433595a272dea1d
Ollie <30156406+geekyboi@users.noreply.github.com>
2025-05-28 18:59:12 +0100

    Update OLA_Install.sh

--- OLA_Install.sh
+++ OLA_Install.sh
@@ -100,50 +100,9 @@ sudo sed -i 's/[ ]*console=serial0,115200[ ]*//g' "$CMDLINE_FILE"
 sudo sed -i 's/  */ /g' "$CMDLINE_FILE"
 sudo sed -i 's/^ *//;s/ *$//' "$CMDLINE_FILE"
 
-# Configure OLA UART plugin
-UART_Config="/home/pi/.ola/ola-uartdmx.conf"
-if curl -sSL https://raw.githubusercontent.com/geekyboi/OLA_DiscoPi/main/ola-uartdmx.conf -o ola-uartdmx.conf; then
-    sudo cp ola-uartdmx.conf "$UART_Config"
-    rm ola-uartdmx.conf
-else
-    echo "❌ Download failed. Uart configuration not updated."
-fi
-
-# Configure OLA Universe Creation
-UART_Config="/home/pi/.ola/ola-universe.conf"
-if curl -sSL https://raw.githubusercontent.com/geekyboi/OLA_DiscoPi/main/ola-universe.conf -o ola-universe.conf; then
-    sudo cp ola-universe.conf "$UART_Config"
-    rm ola-uartdmx.conf
-else
-    echo "❌ Download failed. Universe not created."
-fi
-
-# Configure OLA Universe Port
-UART_Config="/home/pi/.ola/ola-port.conf"
-if curl -sSL https://raw.githubusercontent.com/geekyboi/OLA_DiscoPi/main/ola-port.conf -o ola-port.conf; then
-    sudo cp ola-port.conf "$UART_Config"
-    rm ola-uartdmx.conf
-else
-    echo "❌ Download failed. Port not configured."
-fi
-
-# Download ola.service from GitHub
-printf "\n\n📥 Downloading ola.service....\n"
-if curl -sSL https://raw.githubusercontent.com/geekyboi/OLA_DiscoPi/main/ola.service -o ola.service then
-    # Copy OLA service to startup
-    printf "\n\n💡 Enabling OLA service on boot....\n"
-    sudo cp ola.service /lib/systemd/system/ola.service
-    sudo rm ola.service
-    sudo systemctl daemon-reload
-    sudo systemctl enable ola.service
-    sudo systemctl start ola.service
-    echo "Confirming OLA service is running..."
-    systemctl is-active --quiet ola.service && echo "✅ OLA is running." || echo "❌ OLA failed to start."
-else
-    echo "❌ Download failed. Service not enabled."
-fi
+olad -f
 
 # Completed
-printf "\n\n🥳 OLA Compiled and Installed.\n"
-read -r -p "🪄 Reboot now to apply changes? [y/N] " choice 
+printf "\n\n🥳 Step 1 complete - OLA Compiled and Installed.\n"
+read -r -p "🪄 Reboot now to and follow step 2 [y/N] " choice 
 [[ "$choice" =~ ^[Yy]$ ]] && sudo reboot
